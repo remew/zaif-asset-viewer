0554c4c112a13f8cf4952e6f60ee5381ee7344b5
remew <foolishremew@gmail.com>
2017-12-20 01:18:24 +0900

    get access_token

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const serve = require('koa-static');
 const morgan = require('koa-morgan');
 const Router = require('koa-router');
 const session = require('koa-session');
+const agent = require('superagent');
 
 const createRedirectUrl = require('./createRedirectUrl');
 const config = require('./config');
@@ -25,7 +26,34 @@ router.get('/oauth', async (ctx, next) => {
     ctx.redirect(url);
 });
 router.get('/callback', async (ctx, next) => {
-    ctx.body = 'ok';
+    const {state, code} = ctx.request.query;
+    if (state !== ctx.session.id) {
+        return ctx.status = 400;
+    }
+    const data = {
+        code,
+        grant_type: 'authorization_code',
+        client_id: config.apiKeys.clientId,
+        client_secret: config.apiKeys.clientSecret,
+        redirect_uri: 'https://apps.remew.net/zaif-asset-viewer/callback',
+    };
+    console.log(data);
+    const res = await agent.post('https://oauth.zaif.jp/v1/token').type('form').send(data).catch(e => e);
+    if (!res.body) {
+        console.log(res);
+        throw res;
+    }
+    console.log(res.body);
+    ctx.session.accessToken = res.body.access_token;
+    ctx.session.refreshToken = res.body.refresh_token;
+    ctx.redirect('./');
+});
+
+apiRouter.get('/access_token', async (ctx, next) => {
+    const {accessToken} = ctx.session;
+    ctx.body = {
+        access_token: accessToken,
+    };
 });
 
 const SESSION_CONFIG = {
