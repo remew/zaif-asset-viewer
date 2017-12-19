8b57805971f8f21e83891cee3409ea48ee1d77dd
remew <foolishremew@gmail.com>
2017-12-20 00:25:57 +0900

    create api/login_url

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 'use strict';
 
+const crypto = require('crypto');
+const {promisify} = require('util');
+const randomFill = promisify(crypto.randomFill);
+
 const Koa = require('koa');
 const serve = require('koa-static');
 const morgan = require('koa-morgan');
 const Router = require('koa-router');
 const session = require('koa-session');
 
+const createRedirectUrl = require('./createRedirectUrl');
 const config = require('./config');
 
 const app = new Koa;
 const apiRouter = new Router({prefix: '/api'});
 const callbackRouter = new Router({prefix: '/callback'});
+
+apiRouter.get('/login_url', async (ctx, next) => {
+    const buf = await randomFill(Buffer.alloc(16));
+    const id = buf.toString('hex');
+    ctx.session.id = id;
+    const url = createRedirectUrl(id, config.apiKeys.clientId);
+    ctx.body = url;
+});
 
 callbackRouter.get('/', async (ctx, next) => {
     ctx.body = 'ok';
---
+++ createRedirectUrl.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = function createRedirectUrl(state, clientId) {
+    const urlBase = 'https://zaif.jp/oauth';
+    const data = {
+        client_id: clientId,
+        state,
+        response_type: 'code',
+        scope: 'info',
+        redirect_url: 'https://apps.remew.net/zaif-asset-viewer/callback',
+    };
+
+    return `${urlBase}?${Object.entries(data).map(([key, value]) => `${key}=${encodeURIComponent(value)}`).join('&')}`;
+};
+
