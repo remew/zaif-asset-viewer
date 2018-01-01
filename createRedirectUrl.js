a2adbae0389d74556f22f4505582575a303e7ef5
remew <foolishremew@gmail.com>
2018-01-01 11:28:40 +0900

    import config file

--- createRedirectUrl.js
+++ createRedirectUrl.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const config = require('./config');
+
 module.exports = function createRedirectUrl(state, clientId) {
     const urlBase = 'https://zaif.jp/oauth';
     const data = {
